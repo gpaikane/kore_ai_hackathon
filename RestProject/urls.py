60c953346d786e375fa2a986f130814558ee9595
gpaikane <96648465+gpaikane@users.noreply.github.com>
2022-07-22 15:10:30 +0530

    Update urls.py

--- RestProject/urls.py
+++ RestProject/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appointments.urls'))
-
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
