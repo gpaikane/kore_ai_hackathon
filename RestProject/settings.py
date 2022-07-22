4bae8f9e7a9c7e4031dd417a9f3d917e419c28e8
gpaikane <guruppk46@gmail.com>
2022-07-22 15:00:36 +0530

    added basic code for heroku deployment

--- RestProject/settings.py
+++ RestProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5e1pg$hkpq_)3n**6b&!npv)jpsx!@@=#ngnv*&2y+rkyu%7*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','rest-for-bot.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
